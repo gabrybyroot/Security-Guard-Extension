ce8d09cda62078646ce8a947a8831c1f5bc10e7f
Gabriele <157123715+gabrybyroot@users.noreply.github.com>
2024-01-23 21:07:28 +0100

    warning.js

--- warning.js
+++ warning.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Listener per il bottone "Torna indietro"
+  // Listener for the "Back "button"
   document.getElementById("cancel").addEventListener("click", function() {
       history.back();
   });
 
-  // Listener per il bottone "Vai avanti"
+  // Listener for the "Go Ahead button"
   document.getElementById("proceed").addEventListener("click", function() {
       const urlParams = new URLSearchParams(window.location.search);
       const originalUrl = urlParams.get("url");
